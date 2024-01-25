fdd50aec28a2afd016cb6b84c2f3f0b34c3ea651
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-24 20:03:41 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace GA_StringParsing_Ishak
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string string12 = "12"; 
+            int number12 = int.Parse(string12); 
+            int addValue = number12 + number12; 
+            Console.WriteLine(addValue);
+
+            Console.Write("Enter your first number: "); 
+            string userNum1 = Console.ReadLine(); 
+            Console.Write("Enter your second number: ");
+            string userNum2 = Console.ReadLine();
+
+            int number1 = int.Parse(userNum1);
+            int number2 = int.Parse(userNum2);
+
+            int sum = number1 + number2;
+            Console.WriteLine($"{number1} + {number2} = {sum}");
+            
+
+
+
+
+
+
+
+
+
         }
     }
 }
